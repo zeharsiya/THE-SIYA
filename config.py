46b11a7f064ab1d7ac74d0913f6b82d7bd30120c
zeharsiya <151906273+zeharsiya@users.noreply.github.com>
2023-11-25 22:42:20 +0530

    Update config.py

--- config.py
+++ config.py
@@ -31,7 +31,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/zeharsiya/Siya",
+    "https://github.com/zeharsiya/THE-SIYA",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "Siya")
 GIT_TOKEN = getenv(
